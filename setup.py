6ea515831c717e3d23ae622245647c18f74b4fa3
Bobo <spocky@post.cz>
2024-12-01 20:15:50 +0100

    fix of RSI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zentools",
-    version="0.1.13",
+    version="0.1.14",
     description="A library for algorithmic analysis using Pandas and NumPy",
     author="Bobo",
     author_email="theb02b04@gmail.com",
