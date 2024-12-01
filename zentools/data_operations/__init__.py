43aa2cbbcccc61d0b6fac79c2e8032ee741b4439
Bobo <spocky@post.cz>
2024-12-01 19:57:35 +0100

    added RSI stuff

--- zentools/data_operations/__init__.py
+++ zentools/data_operations/__init__.py
@@ -1,6 +1,9 @@
 from .analysis_tools import *
-from .technical_tools import add_macd
+from .technical_tools import add_macd, add_rsi, add_ewm_rsi
 from .dataframe_tools import slice_dataframe_by_date
 
 # Expose the public API for this submodule
-__all__ = ["add_macd", "slice_dataframe_by_date"]
+__all__ = ["add_macd",
+           "slice_dataframe_by_date",
+           "add_rsi",
+           "add_ewm_rsi"]
